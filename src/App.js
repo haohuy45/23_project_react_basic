06f8cac6c7cb6d5165f3ddd6d65f4b868288e8fc
haohuy45 <haohuy45@gmail.com>
2021-09-30 08:33:10 +0700

    birthday-reminder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import person from './data'
 
 function App() {
+  const [numbers, setNumbers] = useState(person)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='container'>
+        <h3>{numbers.length} birthdays today</h3>
+        {numbers.map((people) => {
+          const {id, name, age, image} = people
+          return (
+            <article className='person' key={id}>
+              <img src={image} alt='Bertie Yates' />
+              <div>
+                <h4>{name}</h4>
+                <p>{age} years</p>
+              </div>
+            </article>
+          )
+        })}
+        <button className='btn' onClick={()=>setNumbers([])}>Clear All</button>
+      </section>
+    </main>
+  )
 }
 
 export default App;
